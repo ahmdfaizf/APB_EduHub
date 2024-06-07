e92db83f5970fd4e6058c3f24e5b255238afe06a
MaheshRaga <maheshraganesta@gmail.com>
2024-06-07 20:47:29 +0700

    Register page

--- APB_APP/edu_hub/lib/page/register_page.dart
+++ APB_APP/edu_hub/lib/page/register_page.dart
@@ -1,3 +1,6 @@
+import 'package:edu_hub/components/my_button.dart';
+import 'package:edu_hub/components/my_textfield.dart';
+import 'package:edu_hub/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +11,120 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final passwordController = TextEditingController();
+  String? selectedRole;
+
+  void registerUser() {
+    // Kosong karena baru ui
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [AppPalet.primary, Color.fromARGB(193, 99, 171, 207)]),
+          ),
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                "assets/LogoEduHub.png",
+                height: 200,
+              ),
+              const Text(
+                'Create an Account',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: nameController,
+                hintText: 'Name',
+                obscureText: false,
+              ),
+              const SizedBox(height: 15),
+              MyTextField(
+                controller: emailController,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+              const SizedBox(height: 15),
+              MyTextField(
+                controller: phoneController,
+                hintText: 'Phone Number',
+                obscureText: false,
+              ),
+              const SizedBox(height: 15),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  hintText: 'Role',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                  border: OutlineInputBorder(),
+                ),
+                value: selectedRole,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedRole = newValue;
+                  });
+                },
+                items: const [
+                  DropdownMenuItem(
+                    value: 'Student',
+                    child: Text('Student'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Teacher',
+                    child: Text('Teacher'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              const SizedBox(height: 25),
+              MyButton(
+                onTap: registerUser,
+              ),
+              const SizedBox(height: 25),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text('or Logs In here'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
